2e02bc7241f56aab0bb0621e9780f9105e45c782
钰辰 <1343257845@qq.com>
2019-06-07 16:50:44 +0800

    shit

--- TTMS/settings.py
+++ TTMS/settings.py
@@ -75,33 +75,33 @@ WSGI_APPLICATION = 'TTMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-
-        'NAME': 'ttms',  # 你的数据库名称
-
-        'USER': 'root',  # 你的数据库用户名
-
-        'PASSWORD': 'yuchen125',  # 你的数据库密码
-
-        'HOST': '',  # 你的数据库主机，留空默认为localhost
-
-        'PORT': '3306',  # 你的数据库端口
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+#
+# DATABASES = {
+#
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.mysql',
+#
+#         'NAME': 'ttms',  # 你的数据库名称
+#
+#         'USER': 'root',  # 你的数据库用户名
+#
+#         'PASSWORD': 'yuchen125',  # 你的数据库密码
+#
+#         'HOST': '',  # 你的数据库主机1，留空默认为localhost
+#
+#         'PORT': '3306',  # 你的数据库端口
+#
+#     }
+#
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
