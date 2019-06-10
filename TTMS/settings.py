479867ff26ac3f5af8ec7b66e1133dba4624bd6e
钰辰 <1343257845@qq.com>
2019-06-10 11:37:04 +0800

    update setting_debug

--- TTMS/settings.py
+++ TTMS/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#88^yb3cj%$qxuj408xpm#ecky(ia^r-2*!=&qtmj_k)++v72o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['101.132.191.43']
+ALLOWED_HOSTS = ['101.132.191.43 ']
 
 # Application definition
 
